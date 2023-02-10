bcd54aecfb61108340897ad8ed974965487a02cf
eastlooo <nupic7@gmail.com>
2023-02-10 18:49:56 +0900

    Initial Commit

---
+++ FlashNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashNotes
+//
+//  Created by 정동천 on 2023/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
