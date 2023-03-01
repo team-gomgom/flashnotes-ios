df9e07967e33c0bd4bb6bab9c798e83320d2df13
eastlooo <nupic7@gmail.com>
2023-03-01 18:12:47 +0900

    FND-002 Feat: L10n 생성

---
+++ Core/Sources/Resource/L10n.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public enum L10n {}
+
+private extension L10n {
+  static func tr(_ key: String, args: CVarArg...) -> String {
+    let table = "Localizable"
+    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
