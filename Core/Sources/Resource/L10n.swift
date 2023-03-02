08ec13fae65a1d937b3a9fae06492b9718b0112f
eastlooo <nupic7@gmail.com>
2023-03-03 02:53:42 +0900

    FND-010 Feat: NoteCreation Localization 추가

--- Core/Sources/Resource/L10n.swift
+++ Core/Sources/Resource/L10n.swift
@@ -2,9 +2,17 @@ import Foundation
 
 public enum L10n {
   public enum Action {
+    public static let done = tr("action.done")
     public static let cancel = tr("action.cancel")
     public static let addPage = tr("action.page_add")
     public static let deleteNote = tr("action.note_delete")
+  }
+
+  public enum Alert {
+    public enum NoteCreation {
+      public static let title = tr("alert.note_creation.title")
+      public static let message = tr("alert.note_creation.message")
+    }
   }
 }
 
